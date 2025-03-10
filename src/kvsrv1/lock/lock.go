18418d3b44d91c2dd1b1e0e2b1393e82350b8486
Jimmy xu <jimmygns@me.com>
2025-03-09 21:51:32 -0400

    distributed lock implementation

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,19 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
+)
+
+type lockState string
+
+const (
+	unlocked lockState = "-1"
 )
 
 type Lock struct {
@@ -11,6 +23,20 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	lockKey string
+
+	acquired        bool
+	acquiredVersion rpc.Tversion
+	lockID          int
+}
+
+func lockStateToInt(s lockState) int {
+	i, _ := strconv.ParseInt(string(s), 10, 32)
+	return int(i)
+}
+
+func intToLockState(i int) lockState {
+	return lockState(strconv.Itoa(i))
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +45,63 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
+	lk := &Lock{ck: ck, lockKey: l, lockID: rand.Intn(10000)}
 	// You may add code here
 	return lk
 }
 
 func (lk *Lock) Acquire() {
+	if lk.acquired {
+		return
+	}
 	// Your code here
+	for {
+		state, version, err := lk.ck.Get(lk.lockKey)
+		if err == rpc.ErrNoKey {
+			state = string(intToLockState(lk.lockID))
+			err = lk.ck.Put(lk.lockKey, state, 0)
+			if err == rpc.OK {
+				lk.acquired = true
+				lk.acquiredVersion = 1
+				break
+			} else if err == rpc.ErrMaybe {
+				s, v, _ := lk.ck.Get(lk.lockKey)
+				if v == 1 && s == string(intToLockState(lk.lockID)) {
+					lk.acquired = true
+					lk.acquiredVersion = 1
+					break
+				}
+			}
+		} else if err == rpc.OK {
+			if state == string(unlocked) {
+				err = lk.ck.Put(lk.lockKey, string(intToLockState(lk.lockID)), version)
+				if err == rpc.OK {
+					lk.acquired = true
+					lk.acquiredVersion = version + 1
+					break
+				} else if err == rpc.ErrMaybe {
+					s, v, _ := lk.ck.Get(lk.lockKey)
+					if v == version+1 && s == string(intToLockState(lk.lockID)) {
+						lk.acquired = true
+						lk.acquiredVersion = version + 1
+						break
+					}
+				}
+			}
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	if !lk.acquired {
+		return
+	}
+	if err := lk.ck.Put(lk.lockKey, string(unlocked), lk.acquiredVersion); err == rpc.OK || err == rpc.ErrMaybe {
+		lk.acquired = false
+		lk.acquiredVersion = 0
+		return
+	}
+	fmt.Printf("Release failed\n")
 }
--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -6,8 +6,13 @@ import (
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
+
+type Item struct {
+	Value   string
+	Version rpc.Tversion
+}
 
 const Debug = false
 
@@ -18,15 +23,17 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	mem map[string]*Item
 }
 
 func MakeKVServer() *KVServer {
-	kv := &KVServer{}
+	kv := &KVServer{
+		mem: make(map[string]*Item),
+	}
 	// Your code here.
 	return kv
 }
@@ -34,7 +41,17 @@ func MakeKVServer() *KVServer {
 // Get returns the value and version for args.Key, if args.Key
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	key := args.Key
+	if item, ok := kv.mem[key]; ok {
+		reply.Value = item.Value
+		reply.Version = item.Version
+		reply.Err = rpc.OK
+	} else {
+		reply.Err = rpc.ErrNoKey
+	}
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -42,13 +59,31 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // If the key doesn't exist, Put installs the value if the
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	key := args.Key
+	if item, ok := kv.mem[key]; ok {
+		if item.Version == args.Version {
+			item.Value = args.Value
+			item.Version++
+			reply.Err = rpc.OK
+		} else {
+			reply.Err = rpc.ErrVersion
+		}
+	} else {
+		if args.Version == 0 {
+			kv.mem[key] = &Item{Value: args.Value, Version: 1}
+			reply.Err = rpc.OK
+		} else {
+			reply.Err = rpc.ErrNoKey
+		}
+	}
 }
 
 // You can ignore Kill() for this lab
 func (kv *KVServer) Kill() {
 }
-
 
 // You can ignore all arguments; they are for replicated KVservers
 func StartKVServer(ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []tester.IService {
